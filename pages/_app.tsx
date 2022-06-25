3a90fcae3c416c5a2c51ecfb0db849ab513c1ace
Adrian Developer <adrian333dev@gmail.com>
2022-06-26 01:05:02 +0400

    mui set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider,CssBaseline } from '@mui/material';
+import { CacheProvider, EmotionCache } from '@emotion/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import theme from '../styles/theme/theme';
+import createEmotionCache from '../utility/createEmotionCache';
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+	emotionCache?: EmotionCache;
 }
 
-export default MyApp
+export default function MyApp(props: MyAppProps) {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+	return (
+		<CacheProvider value={emotionCache}>
+			<Head>
+				<meta name='viewport' content='initial-scale=1, width=device-width' />
+			</Head>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</CacheProvider>
+	);
+}
